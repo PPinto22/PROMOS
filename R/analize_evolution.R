ce8e19ec183b01e842c457474d0c9545d227d8df
Pedro Pinto <pedropinto24@hotmail.com>
2018-05-01 22:50:20 +0100

    Visualization of evolution with R

--- R/analize_evolution.R
+++ R/analize_evolution.R
@@ -1,7 +1,17 @@
-data <- read.csv(file='../results/neat_2018-05-01_18:57:40_evaluations.csv', header=TRUE, sep=',')
+df <- read.csv(file='../results/neat_2018-05-01_18:57:40_evaluations.csv', header=TRUE, sep=',')
 
-gens_max <- aggregate(. ~ generation, data = data, max)
+gens_max <- aggregate(. ~ generation, data = df, max)
 
-plot(gens_max$generation, gens_max$fitness, xlab = "Generations", ylab = "Fitness (AUC)")
-plot(gens_max$generation, gens_max$neurons)
-plot(gens_max$generation, gens_max$connections)
+#install.packages("ggplot2")
+library(ggplot2)
+
+# Plot fitness (neurons/connections) over generations
+ggplot(gens_max, aes(generation, fitness)) + 
+  geom_line() +
+  labs(x="Generation", y="Fitness (AUC)")
+
+# Plot complexity (neurons/connections) over generations
+ggplot(gens_max, aes(generation)) + 
+  geom_line(aes(y = neurons, col = "neurons")) + 
+  geom_line(aes(y = connections, col = "connections")) +
+  labs(x="Generation", y="Network Complexity", colour="")
