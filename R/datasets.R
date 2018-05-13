6db80f06339dd7b4701c7f4ae151ba68e4114931
Pedro Pinto <pedropinto24@hotmail.com>
2018-05-13 22:19:53 +0100

    Separate train/test data-sets

---
+++ R/datasets.R
@@ -0,0 +1,11 @@
+#install.packages("rstudioapi")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+data <- read.csv("../data/data.csv", header = TRUE, sep = ";")
+
+data.train.index = sample(1:nrow(data),size=ceiling(0.7*nrow(data))) 
+data.train <- data[data.train.index,] 
+data.test <- data[-data.train.index,]
+
+write.table(data.train, file="../data/data_train.csv", row.names = FALSE, sep = ";")
+write.table(data.test , file="../data/data_test.csv",  row.names = FALSE, sep = ";")
