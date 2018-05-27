e2c92f7fbad8a862526feee4aa9cf59f66168f41
Pedro Pinto <pedropinto24@hotmail.com>
2018-05-27 18:45:20 +0100

    Update run_neat_sampling.sh

--- scripts/run_neat_sampling.sh
+++ scripts/run_neat_sampling.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-evolver="../src/py/evolver.py"
-options="-d ../data/data_train.csv -t ../data/data_test.csv -o ../results/NEAT/samples_30 -m neat -T 30 -p 54"
+evolver="python ../src/py/evolver.py"
+args="../data/data_train.csv"
+options="-t ../data/data_test.csv -o ../results/TEMP -m neat -T 30 -p 54"
 
 # Do 30 runs
 for i in {1..30} 
 do
-	python ${evolver} ${options} -s 100 --id="100(${i})"
-	python ${evolver} ${options} -s 1000 --id="1K(${i})"
-	python ${evolver} ${options} -s 10000 --id="10K(${i})"
-	python ${evolver} ${options} --id="ALL(${i})"
+	${evolver} ${args} ${options} -s 100 --id="100(${i})"
+	${evolver} ${args} ${options} -s 1000 --id="1K(${i})"
+	${evolver} ${args} ${options} -s 10000 --id="10K(${i})"
+	${evolver} ${args} ${options} --id="ALL(${i})"
 done
 
 exit 0
