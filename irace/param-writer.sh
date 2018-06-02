4409ea58b1b96dbc74cdfd369b3d33d02ba4aab8
Pedro Pinto <pedropinto24@hotmail.com>
2018-06-02 21:58:48 +0100

    Add activation functions parameter on irace

--- irace/param-writer.sh
+++ irace/param-writer.sh
@@ -3,26 +3,47 @@
 # Input example: PopulationSize=100 MutationRate=0.05
 arguments=$*
 
+activation_functions() {
+  usigmoid=0; ugauss=0; usine=0; relu=0
+  case $1 in
+    0)  usigmoid=1;;
+    1)  ugauss=1;;
+    2)  usine=1;;
+    3)  relu=1;;
+    4)  usigmoid=0.5; ugauss=0.5;;
+    5)  usigmoid=0.5; usine=0.5;;
+    6)  usigmoid=0.5; relu=0.5;;
+    7)  ugauss=0.5; usine=0.5;;
+    8)  ugauss=0.5; relu=0.5;;
+    9)  usine=0.5; relu=0.5;;
+    10) usigmoid=0.334; ugauss=0.333; usine=0.333;;
+    11) usigmoid=0.334; ugauss=0.333; relu=0.333;;
+    12) usigmoid=0.334; usine=0.333; relu=0.333;;
+    13) ugauss=0.334; usine=0.333; relu=0.333;;
+    14) usigmoid=0.25; ugauss=0.25; usine=0.25; relu=0.25;;
+  esac
+  echo "ActivationFunction_UnsignedSigmoid_Prob ${usigmoid}"
+  echo "ActivationFunction_UnsignedGauss_Prob ${ugauss}"
+  echo "ActivationFunction_UnsignedSine_Prob ${usine}"
+  echo "ActivationFunction_Relu_Prob ${relu}"
+}
+
 # Read parameters into array
 IFS=' ' read -r -a params <<< $arguments
 
-echo "NEAT_ParametersStart"
-# Print default parameters
-echo "
-MutateNeuronTraitsProb 0
-MutateLinkTraitsProb 0
-MutateGenomeTraitsProb 0
-ActivationFunction_UnsignedGauss_Prob 0.333
-ActivationFunction_UnsignedSigmoid_Prob 0.334
-ActivationFunction_Relu_Prob 0.333
-"
-
+echo -e "NEAT_ParametersStart\n"
 # Print parameters received as input
 for p in "${params[@]}"
 do
-    # Replace '=' with a space and print
-    echo "${p/=/ }"
+    # Split the parameter and value by the '='
+    psplit=(${p/=/ })
+    param=${psplit[0]}
+    value=${psplit[1]}
+    case ${param} in
+      Activation) # Convert activation function parameter
+        echo | activation_functions $value;;
+      *) # Default case: print "param value"
+        echo $param $value;;
+    esac
 done
-
-echo "
-NEAT_ParametersEnd"
+echo -e "\nNEAT_ParametersEnd"
