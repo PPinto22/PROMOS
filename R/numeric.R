862644660cc9577daab7548f1cd68ac13509e0d6
Pedro Pinto <pedropinto24@hotmail.com>
2018-06-05 22:28:32 +0100

    Add inverse document frequency method for catergorial to numeric conversion

---
+++ R/numeric.R
@@ -0,0 +1,41 @@
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+library(jsonlite)
+library(data.table)
+library(hash)
+
+TARGET <- "target"
+POSITIVE_CLASS <- "Sale"
+DATE <- "date_added_utc"
+
+# Read data
+dt <- as.data.table(fromJSON('../data/ColetasPROMOSBESTTreated2018-05-16.json'))
+dt$id <- NULL
+
+# Rename country_name to country
+names(dt)[names(dt) == 'country_name'] <- 'country'
+
+# Replace empty cells with the keyword EMPTY
+dt <- dt[, lapply(.SD, function(x) replace(x, which(x==''), 'empty'))]
+
+# Transform TARGET from categorical to numeric
+dt[[TARGET]] <- replace(dt[[TARGET]], dt[[TARGET]]==POSITIVE_CLASS, 1)
+dt[[TARGET]] <- replace(dt[[TARGET]], dt[[TARGET]]!=POSITIVE_CLASS, 0)
+dt[[TARGET]] <- as.numeric(dt[[TARGET]])
+
+idfDeep=function(v)
+{
+  tf=table(v)
+  N=length(v)
+  idf=log(N/tf)
+  idf_hash=hash(keys=names(idf), values=idf)
+  
+  return(sapply(v, function(x){idf_hash[[as.character(x)]]}))
+}
+# v <- c('a','b','b','c','c','c','d','d','d','d')
+# idfDeep(v)
+
+cols <- colnames(dt)[!colnames(dt) %in% c(TARGET, DATE)]
+dt[, (cols) := lapply(.SD, idfDeep), .SDcols = cols]
+
+write.table(dt, file = '../data/week1_best.csv', sep=',', row.names = FALSE)
