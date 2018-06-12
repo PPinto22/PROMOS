e5efbf526875dc128caf935e9315ad94a34848d6
Pedro Pinto <pedropinto24@hotmail.com>
2018-06-12 20:34:35 +0100

    Update scripts

--- scripts/run_neat_sliding_window.sh
+++ scripts/run_neat_sliding_window.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 evolver="python ../src/py/evolver.py"
 args="../data/week1_best/all.csv"
-options="-P ../params/neat0.txt -o ../results/window -m neat -g750 -s1000 -p7 -W96 -w24 -S24 --test-fitness --quiet"
-runs=16
+options="-P ../params/irace2.txt -o ../results/window_irace2 -m neat -g750 -s1000 -p7 -W96 -w24 -S24 --test-fitness --quiet"
+runs=32
 parallel=8
 
 for i in $(seq 1 $runs)
