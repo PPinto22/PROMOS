680723eceae0d9151d675a07ed681d81d5b5881e
Pedro Pinto <pedropinto24@hotmail.com>
2018-06-12 20:36:41 +0100

    Testing the configurations found by irace

--- irace/output/analyser.R
+++ irace/output/analyser.R
@@ -1,3 +1,18 @@
 # setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 load('irace.Rdata')
+
+results <- iraceResults$testing$experiments
+results <- results[, seq(1, 4)]
+results <- apply(results, c(1,2), '*', -1) # Flip from negative to positive
+colnames(results) <- paste("Config", seq(1, ncol(results)))
+
+conf <- gl(ncol(results), nrow(results), labels=colnames(results))
+
+sink('ttest.txt')
+pairwise.t.test(as.vector(results), conf,  paired=TRUE)
+sink()
+
+png(filename = 'test_bp.png')
+boxplot(results, ylab='Fitness')
+dev.off()
