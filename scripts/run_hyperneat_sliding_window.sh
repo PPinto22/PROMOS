d1049e5910a5b068e3beb7f4aa52e8dc1be63ea0
Pedro Pinto <pedropinto24@hotmail.com>
2018-06-13 22:58:58 +0100

    Add script for hyperneat with sliding window

---
+++ scripts/run_hyperneat_sliding_window.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+evolver="python ../src/py/evolver.py"
+args="../data/week1_best/all.csv"
+options="-P ../params/irace2.txt -o ../results/hyperneat_window -m hyperneat -x0 -g1000 -s1000 -p7 -W96 -w24 -S24 --test-fitness --quiet"
+runs=32
+parallel=8
+
+for i in $(seq 1 $runs)
+do
+	echo "Run $i..."
+	( $evolver $args $options -s 100 --id="windows(${i})" ) &
+	if (( $i % $parallel == 0 )); then wait; fi
+done
+wait
+exit 0
