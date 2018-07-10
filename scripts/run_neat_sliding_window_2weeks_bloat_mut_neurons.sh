3c9933cdca870186ac9979c14b03cca03010fad8
Pedro Pinto <pedropinto24@hotmail.com>
2018-07-10 17:17:12 +0100

    Add script

---
+++ scripts/run_neat_sliding_window_2weeks_bloat_mut_neurons.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+evolver="python ../src/py/evolver.py"
+args="../data/2weeks/best_idf.csv"
+id="bloat_mut_neurons"
+options="-P ../params/irace2.txt -o ../results/${id}/ -m neat -g750 -s2500 -p13 -W120 -w24 -S24 --test-fitness -b ../params/bloat_mut_neurons.cfg"
+runs=4
+parallel=4
+
+for i in $(seq 1 $runs)
+do
+	echo "Run $i..."
+	( $evolver $args $options --id="${id}(${i})" ) &
+	if (( $i % $parallel == 0 )); then wait; fi
+done
+wait
+exit 0
