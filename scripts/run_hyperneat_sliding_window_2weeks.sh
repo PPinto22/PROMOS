394c3a241b7b1dc61d541e72c1723616e07e0d95
Pedro Pinto <pedropinto24@hotmail.com>
2018-07-11 14:13:21 +0100

    Update scripts

---
+++ scripts/run_hyperneat_sliding_window_2weeks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+evolver="python ../src/py/evolver.py"
+args="../data/2weeks/best_idf.csv"
+id="hn_2weeks_idf"
+options="-P ../params/irace2.txt -o ../results/${id}/ -m hyperneat x0 -g750 -s2500 -p10 -W120 -w24 -S24 --test-fitness"
+runs=4
+parallel=4
+
+for i in $(seq 1 $runs)
+do
+	echo "Run $i..."
+	( $evolver $args $options --id="${id}(${i})" ) &
+	if (( $i % $parallel == 0 )); then wait; fi
+done
+wait
+exit 0
