27448d13d57ef79551cd4bda7fe5e0b62c9acc3f
Pedro Pinto <pedropinto24@hotmail.com>
2018-08-23 11:44:58 +0100

    Update script

--- scripts/run_neat_processes.sh
+++ scripts/run_neat_processes.sh
@@ -6,7 +6,7 @@ runs=1
 
 for i in $(seq 1 $runs)
 do
-	for p in {1,2,4,8,16,32,44,56} 
+	for p in {1,2,4,8,16,30,40,50} 
 	do
 		echo "Processes: $p | Run: $i..."
 		( $evolver $args $options -p$p --id="$p($i)" )
