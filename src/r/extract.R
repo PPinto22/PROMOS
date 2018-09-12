5cf4ed244071e8d22cb4eaf9866d4acc98bdd09e
Pedro Pinto <pedropinto24@hotmail.com>
2018-09-12 15:12:33 +0100

    Fix optparse working dir option

--- src/r/extract.R
+++ src/r/extract.R
@@ -4,7 +4,7 @@ library(doParallel)
 library(optparse)
 
 option_list = list(
-  make_option(c("-w","--working-dir"), type="character", default=".", help="working directory", metavar="DIR"),
+  make_option(c("-w","--workingdir"), type="character", default=".", help="working directory", metavar="DIR"),
   make_option(c("-s","--sales", type="character", default=NULL, help="path where to save the extracted sales", metavar="FILE")),
   make_option(c("-r","--redirects", type="character", default=NULL, help="path where to save the extracted redirects", metavar="FILE")),
   make_option(c("-t","--time"), type="double", default = 6, help="collection time duration in hours", metavar = "H")
@@ -12,8 +12,8 @@ option_list = list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
-dir.create(file.path(opt$working-dir), recursive=TRUE, showWarnings=FALSE)
-setwd(opt$working-dir)
+dir.create(file.path(opt$workingdir), recursive=TRUE, showWarnings=FALSE)
+setwd(opt$workingdir)
 
 
 #GLOBAL VARIABLES SETTINGS
