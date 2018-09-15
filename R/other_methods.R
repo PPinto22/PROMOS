a83c279c4f5325a2c7578043c9a422dbda44d26b
Pedro Pinto <pedropinto24@hotmail.com>
2018-09-15 19:39:33 +0100

    Testing predictive methods with rminer

--- R/other_methods.R
+++ R/other_methods.R
@@ -1,12 +1,14 @@
 # setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 library(data.table)
+library(rminer)
 library(pROC)
 
-TRAIN_FILES <- c('../data/2weeks/best_idf_train.csv')
-TEST_FILES <- c('../data/2weeks/best_idf_test.csv')
-ENCODINGS <- c('idf')
-MODES <- c('best')
+# Train; test; encoding; mode
+INSTANCES <- list(
+  c('../data/2weeks/best_idf_train.csv', '../data/2weeks/best_idf_test.csv', 'idf', 'best')
+)
+ALGORITHMS <- c('lr', 'randomForest', 'svm', 'mlp', 'xgboost')
 options(digits=5)
 
 read_data <- function(path){
@@ -15,26 +17,22 @@ read_data <- function(path){
   return(dt)
 }
 
+train_dts <- lapply(INSTANCES, function(x) read_data(x[1]))
+test_dts <- lapply(INSTANCES, function(x) read_data(x[2]))
+
 summary_df <- data.frame(algorithm=character(), mode=character(), encoding=character(), auc=double(), time=double())
-
-train_dts <- lapply(TRAIN_FILES, read_data)
-test_dts <- lapply(TEST_FILES, read_data)
-
-# linear regression
-for (i in 1:length(TRAIN_FILES)) {
-  train_dt = train_dts[[i]]
-  test_dt = test_dts[[i]]
-  mode = MODES[[i]]
-  encoding = ENCODINGS[[i]]
   
-  time <- system.time(glm_model <- glm(target ~ ., train_dt,  family = "binomial"))[[3]]
-  predictions <- predict(glm_model, test_dt)
-  auc_score <- auc(roc(test_dt$target, predictions))
-  summary_row <- list(algorithm='lr', mode=mode, encoding=encoding, auc=auc_score, time=time)
-  summary_df <- rbind(summary_df, summary_row)
+for(alg in ALGORITHMS){
+  for (i in 1:length(INSTANCES)){
+    train_dt = train_dts[[i]]
+    test_dt = test_dts[[i]]
+    mode = INSTANCES[[i]][3]
+    encoding = INSTANCES[[i]][4]
+    
+    time <- system.time(model <- fit(target ~ ., train_dt,  model = alg, task = 'prob'))[[3]]
+    predictions <- predict(model, test_dt)
+    auc_score <- auc(roc(test_dt$target, predictions))
+    summary_row <- list(algorithm=alg, mode=mode, encoding=encoding, auc=auc_score, time=time)
+    summary_df <- rbind(summary_df, summary_row)
+  }
 }
-
-# xgboost
-# mlp
-# svm
-# random forest
