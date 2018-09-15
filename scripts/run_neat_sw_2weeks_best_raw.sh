e5557beac716571e7d9224a98051bc3a6b4ae10a
Pedro Pinto <pedropinto24@hotmail.com>
2018-09-15 22:50:56 +0100

    Update scripts

--- scripts/run_neat_sw_2weeks_best_raw.sh
+++ scripts/run_neat_sw_2weeks_best_raw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 evolver="python ../src/py/evolver.py"
-options="-d ../data/2weeks/best.csv -P ../params/neat.txt -o ../results/2wks_sw_best_raw -E ../cfg/encoder_raw.cfg -m neat -g1000 -s2000 -p2 --test-fitness -W120 -w24 -S24"
+options="-d ../data/2weeks/best.csv -P ../params/neat.txt -o ../results/2wks_sw_best_raw -E ../cfg/encoder_raw.cfg -m neat -g1000 -s2000 -p2 --test-fitness -W120 -w24 -S24 --quiet"
 runs=4
 parallel=2
 
