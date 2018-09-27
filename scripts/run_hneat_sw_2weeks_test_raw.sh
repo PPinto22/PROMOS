dcbbd4e32fc8051cc0a1a1d82e3ff099f4447256
Pedro Pinto <pedropinto24@hotmail.com>
2018-09-27 20:16:44 +0100

    Add scripts for sliding window in TEST mode

--- scripts/run_hneat_sw_2weeks_test_raw.sh
+++ scripts/run_hneat_sw_2weeks_test_raw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 evolver="python ../src/py/evolver.py"
-options="-d ../data/2weeks/best.csv -P ../params/neat.txt -o ../results/2wks_sw_best_raw -E ../cfg/encoder_raw.cfg -m neat -g1000 -s2000 -p4 --test-fitness -W120 -w24 -S24"
+options="-d ../data/2weeks/test.csv -P ../params/hyperneat.txt -o ../results/2wks_hn_sw_test_raw -E ../cfg/encoder_raw.cfg -m hyperneat -g1000 -s2000 -p6 --test-fitness -W120 -w24 -S24"
 runs=2
 parallel=1
 
@@ -16,7 +16,7 @@ function run() {
 
 for i in $(seq 1 $runs)	
 do
-	run "--id=sw_best_raw(${i})"
+	run "--id=sw_hn_test_raw(${i})"
 done
 wait
 exit 0
